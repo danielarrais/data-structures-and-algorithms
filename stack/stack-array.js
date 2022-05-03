cef1f6b96277335c2ba2d811bafe6c4897d5f3d4
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-05-02 21:51:14 -0300

    Create base stack file

---
+++ stack/stack-array.js
@@ -0,0 +1,29 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(elements) {
+
+    }
+
+    pop() {
+
+    }
+
+    peek() {
+
+    }
+
+    isEmpty() {
+
+    }
+
+    clear() {
+
+    }
+
+    size() {
+
+    }
+}
